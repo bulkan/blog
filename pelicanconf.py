aa9200533d9536d77eb9e09bffd45ef76e908c96
Bulkan Evcimen <bulkan@gmail.com>
2012-12-12 11:23:03 +0000

    added the default pelican files

---
+++ pelicanconf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'bulkan'
+SITENAME = u'Bulkan Savun Evcimen'
+SITEURL = 'http://ec2-23-20-13-253.compute-1.amazonaws.com'
+
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+         ('Python.org', 'http://python.org'),
+         ('Jinja2', 'http://jinja.pocoo.org'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 5
