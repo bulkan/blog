1ee68bf84d48fdb2a5876456506aa76f63a9accb
Bulkan Evcimen <bulkan@gmail.com>
2012-12-16 01:36:34 +1100

    going through blog objects and passing to template

--- importer.py
+++ importer.py
@@ -1,15 +1,35 @@
 import json
 
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('.'))
+
+
+class attrdict(dict):
+    """A dict whose items can also be accessed as member variables."""
+
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+        self.__dict__ = self
+
 
 def main():
 
     f = open("bulkanix_blog.json")
     blogs = json.loads(f.read())
 
-    for blog in blogs:
-        if blog['model'] == 'blog.post':
-            print blog['fields']['title']
+    template = env.get_template('post_template.md')
 
+    for blog in blogs:
+        blog = attrdict(blog)
+        if blog.model == 'blog.post':
+            fields = attrdict(blog.fields)
+
+            print template.render(title=fields.title,
+                date=fields.date_published,
+                tags=fields.tags,
+                slug=fields.slug,
+                body=fields.body_markdown)
 
 if __name__ == "__main__":
     main()
---
+++ post_template.md
@@ -0,0 +1,9 @@
+Title: {{ title }}
+Date: {{ date }}
+Tags: {{ tags }}
+Category: {{ category }}
+Slug: {{ slux }}
+Author: Bulkan Evcimen
+Summary: {{ summary }}
+
+{{ body }}
