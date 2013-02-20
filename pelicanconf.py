2113e059b1b608dc1b1b0a88a22782c30af11a80
Bulkan Evcimen <bulkan@gmail.com>
2013-02-21 00:35:13 +1100

    using cielo theme from aurora chiarello

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,12 +3,15 @@
 
 AUTHOR = u'bulkan'
 SITENAME = u'Bulkan Savun Evcimen'
-SITEURL = 'http://ec2-23-20-13-253.compute-1.amazonaws.com'
+#SITEURL = 'http://ec2-23-20-13-253.compute-1.amazonaws.com'
 
+SITEURL = 'http://localhost:8000'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
+
+CSS_FILE = 'stylesheet.css'
 
 # Blogroll
 LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
@@ -16,8 +19,15 @@ LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
          ('Jinja2', 'http://jinja.pocoo.org'),
          ('You can modify those links in your config file', '#'),)
 
+
+TEMPLATE_PAGES = {'music.html': 'music.html'}
+
+THEME = 'cielo'
+
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 5
+TWITTER_USERNAME = 'bulkanevcimen'
+
+DEFAULT_PAGINATION = 3
