2affb8fa5813edc52789c0c9b1eb345a0d98897c
Bulkan Evcimen <bulkan@gmail.com>
2013-04-29 14:01:18 +0000

    updated conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,21 +3,22 @@
 
 AUTHOR = u'bulkan'
 SITENAME = u'Bulkan Savun Evcimen'
-SITEURL = 'http://ec2-23-20-13-253.compute-1.amazonaws.com'
+SITEURL = 'http://bulkan-evcimen.com'
 
+CSS_FILE = 'stylesheet.css'
 
-TIMEZONE = 'Europe/Paris'
+TEMPLATE_PAGES = {'music.html': 'music.html'}
+
+TWITTER_USERNAME = 'bulkanevcimen'
+
+TIMEZONE = 'Australia/Melbourne'
 
 DEFAULT_LANG = u'en'
-
-# Blogroll
-LINKS = (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-         ('Python.org', 'http://python.org'),
-         ('Jinja2', 'http://jinja.pocoo.org'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 5
+
+THEME = 'cielo/bulkan-evcimen.com'
