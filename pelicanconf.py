6cb9a9491eec23225f4efc16ffb4e91e40a10d75
Bulkan Evcimen <bulkan@gmail.com>
2013-07-31 21:43:04 +1000

    updated blog config to contain static_url

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,17 +7,19 @@ SITEURL = 'http://bulkan-evcimen.com'
 
 CSS_FILE = 'stylesheet.css'
 
-TEMPLATE_PAGES = {'music.html': 'music.html'}
+TEMPLATE_PAGES = {
+    'music.html': 'music.html',
+    'musician.html': 'musician.html'
+}
 
 TIMEZONE = 'Australia/Melbourne'
 
 DEFAULT_LANG = u'en'
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 5
+
+# TODO: move this into a seperate file and import it here
+STATIC_URL = '/cielo/bulkan-evcimen.com/static/'
 
 THEME = 'cielo/bulkan-evcimen.com'
 TWITTER_USERNAME = 'bulkanevcimen'
